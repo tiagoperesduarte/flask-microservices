8b515b8e0ee67337112841668738c5fc1b3528f8
Tiago Peres <psilva.dev@gmail.com>
2020-07-11 19:57:24 -0300

    Atualização das libs do microsserviço de usuário

--- flask-microservices-users/app/config/routes.py
+++ flask-microservices-users/app/config/routes.py
@@ -2,6 +2,8 @@ from flask_smorest import Api
 
 
 def configure_routes(app):
+    app.config['API_TITLE'] = 'API'
+    app.config['API_VERSION'] = 'v1'
     app.config['OPENAPI_VERSION'] = '3.0.2'
 
     from app.resources.user_resource import user_blp
