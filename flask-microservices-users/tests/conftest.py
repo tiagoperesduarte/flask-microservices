4f303aa774fbcf77f30c417730d2780a02426b72
Tiago Peres <psilva.dev@gmail.com>
2020-07-25 21:52:59 -0300

    Adicionado autenticação JWT nos casos de testes do micro serviço de usuário

--- flask-microservices-users/tests/conftest.py
+++ flask-microservices-users/tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
+from flask_jwt_extended import create_access_token
 
 from app import create_app
+from app.models.current_user import CurrentUser
 
 
 @pytest.fixture(scope='session')
@@ -13,4 +15,10 @@ def app():
 
 @pytest.fixture(scope='session')
 def client(app):
-    return app.test_client()
+    with app.app_context():
+        access_token = create_access_token(identity=CurrentUser(id="123456"), expires_delta=False)
+
+        client = app.test_client()
+        client.environ_base['HTTP_AUTHORIZATION'] = f'Bearer {access_token}'
+
+        return client
--- flask-microservices-users/tests/mock/user_mock.py
+++ flask-microservices-users/tests/mock/user_mock.py
@@ -1,14 +1,18 @@
 from app.utils.string_utils import StringUtils
 
 
-def get_mock_user(name=None, email=None):
+def get_mock_user(name=None, email=None, password=None):
     if not name:
         name = f'Usuário {StringUtils.get_random_string()}'
 
     if not email:
         email = f'{StringUtils.sanitize_string(name)}@email.com'
 
+    if not password:
+        password = StringUtils.get_random_string()
+
     return {
         'name': name,
-        'email': email
+        'email': email,
+        'password': password
     }
