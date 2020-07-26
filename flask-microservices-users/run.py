c0520d254e2ec4d18f5987ea6ba66cc73ef6258e
Tiago Peres <psilva.dev@gmail.com>
2020-07-26 19:37:03 -0300

    Adicionado opção para escolha de porta nos micro serviços

--- flask-microservices-users/run.py
+++ flask-microservices-users/run.py
@@ -1,6 +1,7 @@
 from app import create_app
+from app.utils.env_utils import EnvUtils
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=EnvUtils.get_env('FLASK_RUN_PORT'))
